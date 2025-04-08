6e25917d55fb0da666aeb6a0f9f1f0becea937da
vansh080 <140516356+vansh080@users.noreply.github.com>
2025-04-08 21:36:44 +0530

    Update vite.config.ts
    subfolder added

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/my-app/',
+  base: '/hospital-deskboard/',
   plugins: [react()],
   assetsInclude: ['**/*.png', '**/*.jpg', '**/*.jpeg'],
   optimizeDeps: {
